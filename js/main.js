29a9fa0931bbea51d881729a3c88db6ced33032b
Andrew <andreiduffy@gmail.com>
2017-02-19 11:55:09 +0300

    end 1.0

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 $(document).ready(function(){
 	$('.modal').agmodal({
 		effect: 'fade',
-		overlayColor: 'rgba(37, 49, 69, 0.5)'
+		overlayColor: 'rgba(62, 78, 104, 1)'
 	});
 
 	$('.modal-close').on('click',function (e) {
@@ -15,26 +15,6 @@ $(document).ready(function(){
 /***********************
  agmodal END
  ***********************/
-
-/***********************
-Trial modal BEGIN
-***********************/
-function open_trial_modal() {
-	$('.trial-wrapper').addClass('trial-wrapper--visible');
-}
-function close_trial_modal() {
-	$('.trial-wrapper').removeClass('trial-wrapper--visible');
-}
-
-$(document).ready(function() {
-	$('.trial-close').on('click',function (e) {
-		e.preventDefault();
-		close_trial_modal();
-	});
-});
-/***********************
-Trial modal END
-***********************/
 
 
 /***********************
